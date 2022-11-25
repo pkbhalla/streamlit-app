34dc28bb20d3989c9475d2121db654e751007fa3
pkbhalla <96061462+pkbhalla@users.noreply.github.com>
2022-11-26 01:29:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.write("""
+# TDS Module 8 Graded Assignment
+
+For the Multiplication of 2 given numbers using Streamlit and deploying on Heroku
+""")
+
+#Input the numbers
+first_no = st.number_input("Enter first number",min_value=-999999999,max_value=999999999,step=1)
+second_no = st.number_input("Enter second number",min_value=-999999999,max_value=999999999,step=1)
+product = first_no*second_no
+st.write("The product of given numbers is", product)
