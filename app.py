5bb6242ab35b0a0559cc10ae5963856ebfaee5e5
pkbhalla <96061462+pkbhalla@users.noreply.github.com>
2022-11-26 16:08:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,9 @@ st.write("""
 
 ## For the Multiplication of 2 given numbers using Streamlit and deploying on Heroku
 """)
-def body(a):
-  b="The product of given numbers is "
-  return b+str(a)
 #Input the numbers
 first_no = st.number_input("Enter first number")
 second_no = st.number_input("Enter second number")
 product = first_no*second_no
-if st.button("Click to get product of 2 numbers"):
-  print(body(product))
-st.write("The product of given numbers is", product)
+st.write("The product of given numbers is", str(product))
 st.write("By Pratham Bhalla (21f1003052)")
