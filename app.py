7f100691dadaa3da60e1fa562c25b25e27ec170d
pkbhalla <96061462+pkbhalla@users.noreply.github.com>
2022-11-27 19:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-'''import streamlit as st
+import streamlit as st
 
 st.write("""
 # TDS Module 8 Graded Assignment
@@ -11,16 +11,4 @@ first_no = st.number_input("Enter first number")
 second_no = st.number_input("Enter second number")
 product = first_no*second_no
 st.write("The product of given numbers is", str(product))
-st.write("By Pratham Bhalla (21f1003052)")'''
-import streamlit as st
-
-st.write("""
-#TDS Week 8 Assignment
-## Sum of two numbers and depolying using Heroku""")
-
-a = st.number_input("Enter the first number")
-b = st.number_input("Enter the second number")
-
-s = a+b 
-st.write("Sum of the two numbers are:",s)
-st.write("Credits: 21f1005104")
+st.write("By Pratham Bhalla (21f1003052)")
